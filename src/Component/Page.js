b27f133bf744a7043893c01b1a9d5ed060ca9fde
Abhinav <abhinav2003feb@gmail.com>
2023-08-24 18:47:39 +0530

    final commit

---
+++ src/Component/Page.js
@@ -0,0 +1,293 @@
+import React from 'react'
+
+const Page = () => {
+    return (
+        <div>
+            {/* ---------------------------------NAVBAR----------------------------------------------------------------------------------------- */}
+            <div className="w-[1440px] h-[3152px] relative bg-white">
+                <div className="w-[702px] h-[75px] left-[50px] top-[30px] absolute">
+                    <img
+                        className="w-[75px] h-[75px] left-0 top-0 absolute"
+                        src="https://anima-uploads.s3.amazonaws.com/projects/64e665a7f1b61250aab168b5/releases/64e667027704051b825b49eb/img/ekalakaa-5-1@2x.png"
+                    />
+                    <a href='"#' className="left-[175px] top-[24px] absolute text-black text-lg font-medium">
+                        Home
+                    </a>
+                    <a href="#" className="left-[279px] top-[24px] absolute text-black text-lg font-medium">
+                        Benefits
+                    </a>
+                    <a href="#" className="left-[402px] top-[24px] absolute text-black text-lg font-medium">
+                        Services
+                    </a>
+                    <a href="#" className="left-[528px] top-[24px] absolute text-black text-lg font-medium">
+                        Gallery
+                    </a>
+                    <a href="#" className="left-[643px] top-[24px] absolute text-black text-lg font-medium">
+                        Events
+                    </a>
+                </div>
+                <div className="w-10 h-10 left-[1260px] top-[47px] absolute">
+                    <div className="w-10 h-10 left-0 top-0 absolute bg-white rounded-full shadow border border-black border-opacity-20" >
+                        <img style={{ marginTop: "13px", marginLeft: "12px" ,cursor:"pointer"}}
+                           
+                            src="https://anima-uploads.s3.amazonaws.com/projects/64e665a7f1b61250aab168b5/releases/64e667027704051b825b49eb/img/vector-1@2x.png"
+                        />
+                    </div>
+
+                </div>
+                <div className="w-10 h-10 left-[1350px] top-[47px] absolute">
+                    <div className="w-10 h-10 left-0 top-0 absolute bg-white rounded-full shadow border border-black border-opacity-20" >
+                        <img style={{ marginTop: "13px", marginLeft: "12px" ,cursor:"pointer"}}
+
+                            src="https://anima-uploads.s3.amazonaws.com/projects/64e665a7f1b61250aab168b5/releases/64e667027704051b825b49eb/img/vector-2@2x.png"
+                        />
+                    </div>
+                </div>
+
+                {/* ------------------------------------------------------------------------------------------------------------------------------------- */}
+
+                <div className="w-[656px] h-[752px] left-[392px] top-[205px] absolute">
+                    <div className="w-[656px] h-[752px] left-0 top-0 absolute bg-white rounded-[30px] shadow" />
+                    <img
+                        className="w-12 h-12 left-[544px] top-[654px] absolute"
+                        src="https://anima-uploads.s3.amazonaws.com/projects/64e665a7f1b61250aab168b5/releases/64e667027704051b825b49eb/img/globe-1@2x.png"
+                        style={{cursor:"pointer"}}
+                    />
+                    <img
+                        className="w-12 h-12 left-[304px] top-[654px] absolute"
+                        src="https://anima-uploads.s3.amazonaws.com/projects/64e665a7f1b61250aab168b5/releases/64e667027704051b825b49eb/img/linkedin-1@2x.png"
+                        style={{cursor:"pointer"}}
+                    />
+                    <img
+                        className="w-12 h-12 left-[64px] top-[654px] absolute"
+                        src="https://anima-uploads.s3.amazonaws.com/projects/64e665a7f1b61250aab168b5/releases/64e667027704051b825b49eb/img/instagram-1@2x.png"
+                        style={{cursor:"pointer"}}
+                    />
+                    <img
+                        className="w-12 h-12 left-[184px] top-[654px] absolute"
+                        src="https://anima-uploads.s3.amazonaws.com/projects/64e665a7f1b61250aab168b5/releases/64e667027704051b825b49eb/img/facebook-1@2x.png"
+                        style={{cursor:"pointer"}}
+                    />
+                    <img
+                        className="w-12 h-12 left-[424px] top-[654px] absolute"
+                        src="https://anima-uploads.s3.amazonaws.com/projects/64e665a7f1b61250aab168b5/releases/64e667027704051b825b49eb/img/youtube-1@2x.png"
+                        style={{cursor:"pointer"}}
+                    />
+
+
+
+
+                    <div className="w-[656px] h-[200px] left-0 top-0 absolute bg-rose-800 rounded-tl-[30px] rounded-tr-[30px]" />
+                    <img
+                        className="w-[200px] h-[200px] left-[228px] top-[100px] absolute rounded-full"
+                        src="https://anima-uploads.s3.amazonaws.com/projects/64e665a7f1b61250aab168b5/releases/64e667027704051b825b49eb/img/ellipse-461-1@2x.png"
+                    />
+                    <div className="left-[244px] top-[312px] absolute text-black text-2xl font-medium">
+                        Partner Name
+                    </div>
+                    <div className="left-[281px] top-[360px] absolute text-black text-opacity-70 text-lg font-medium">
+                        Profession
+                    </div>
+                    <div className="w-[398px] h-[185px] left-[129px] top-[419px] absolute">
+                        <div className="left-[179px] top-[3px] absolute text-black text-opacity-70 text-sm font-medium">
+                            (Verified)
+                        </div>
+                        <div className="left-[286px] top-[62px] absolute text-black text-opacity-70 text-sm font-medium">
+                            (Verified)
+                        </div>
+                        <div className="w-[167px] h-[27px] left-0 top-0 absolute">
+                            <div className="left-[30px] top-0 absolute text-black text-lg font-medium">
+                                +91 1234567891
+                            </div>
+                        </div>
+                        <div className="w-[274px] h-[27px] left-0 top-[59px] absolute">
+                            <div className="left-[32px] top-0 absolute text-black text-lg font-medium">
+                                randomemail@gmail.com
+                            </div>
+                            <div className="w-5 h-[18px] left-0 top-[8px] absolute"></div>
+                        </div>
+                        <div className="w-[398px] h-[67px] left-0 top-[118px] absolute">
+                            <div className="w-[18px] h-[22px] left-0 top-0 absolute"></div>
+                            <div className="left-[34px] top-0 absolute text-black text-lg font-medium">
+                                123 random street, random city - 123456
+                                <br />
+                                random district
+                                <br />
+                                random state
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+
+
+
+                <div className="w-[180px] h-[50px] p-2.5 left-[630px] top-[1037px] absolute bg-rose-800 rounded-[15px] justify-center items-center gap-2.5 inline-flex">
+                    <div className="text-white text-lg font-semibold" style={{cursor:"pointer"}}>Share Card</div>
+                </div>
+                <div className="w-[180px] h-[50px] p-2.5 left-[630px] top-[2075px] absolute bg-rose-800 rounded-[15px] justify-center items-center gap-2.5 inline-flex">
+                    <div className="text-white text-lg font-semibold" style={{cursor:"pointer"}}>Edit Details</div>
+                </div>
+                <div className="left-[50px] top-[1167px] absolute text-rose-800 text-[32px] font-semibold">
+                    About Us
+                </div>
+                <div className="left-[50px] top-[2205px] absolute text-rose-800 text-[32px] font-semibold">
+                    Our Product Images
+                </div>
+                <div className="w-[1340px] h-[72px] left-[50px] top-[1231px] absolute text-justify text-black text-opacity-70 text-lg font-normal">
+                    Morem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate
+                    libero et velit interdum, ac aliquet odio mattis. Class aptent taciti
+                    sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+                    Curabitur tempus urna at turpis condimentum lobortis. Ut commodo efficitur
+                    neque. Ut diam quam, semper iaculis condimentum ac, vestibulum eu nisl.
+                </div>
+                <div className="w-[1340px] h-[45px] left-[50px] top-[1335px] absolute text-justify text-black text-opacity-70 text-lg font-normal">
+                    Morem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate
+                    libero et velit interdum, ac aliquet odio mattis. Class aptent taciti
+                    sociosqu ad litora torquent per conubia nostra,{" "}
+                </div>
+                <div className="w-[754px] h-[19px] left-[50px] top-[1412px] absolute text-justify text-black text-opacity-70 text-lg font-normal">
+                    Morem ipsum dolor sit amet, consectetur adipiscing elit. Nunc.
+                </div>
+                <div className="w-[1340px] h-[73px] left-[50px] top-[1463px] absolute text-justify text-black text-opacity-70 text-lg font-normal">
+                    Morem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate
+                    libero et velit interdum, ac aliquet odio mattis. Class aptent taciti
+                    sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+                    Curabitur tempus urna at turpis condimentum lobortis. Ut commodo efficitur
+                    neque. Ut diam quam, semper iaculis condimentum ac, vestibulum eu nisl.
+                </div>
+                <div className="w-[561px] h-[379px] left-[447px] top-[1616px] absolute bg-white rounded-[10px] border border-black border-opacity-40" />
+                <div className="left-[479px] top-[1648px] absolute text-black text-opacity-70 text-xl font-semibold">
+                    Category :
+                </div>
+                <div className="left-[479px] top-[1710px] absolute text-black text-opacity-70 text-xl font-semibold">
+                    Products :
+                </div>
+                <div className="left-[479px] top-[1809px] absolute text-black text-opacity-70 text-xl font-semibold">
+                    Products On Sale :
+                </div>
+                <div className="left-[479px] top-[1871px] absolute text-black text-opacity-70 text-xl font-semibold">
+                    Products Sold :
+                </div>
+                <div className="left-[479px] top-[1933px] absolute text-black text-opacity-70 text-xl font-semibold">
+                    Revenue :
+                </div>
+                <div className="left-[712px] top-[1648px] absolute text-rose-800 text-xl font-semibold">
+                    Musical Instruments
+                </div>
+                <div className="left-[712px] top-[1809px] absolute text-rose-800 text-xl font-semibold">
+                    123
+                </div>
+                <div className="left-[712px] top-[1871px] absolute text-rose-800 text-xl font-semibold">
+                    50
+                </div>
+                <div className="left-[712px] top-[1933px] absolute text-rose-800 text-xl font-semibold">
+                    3L
+                </div>
+                <div className="left-[712px] top-[1717px] absolute text-rose-800 text-xl font-semibold">
+                    Piano, Guitar, Violin, Drum
+                    <br />
+                    Trumpet and Flute.{" "}
+                </div>
+                <img
+                    className="w-[348px] h-[348px] left-[134px] top-[2303px] absolute rounded-[30px] shadow"
+                    src="https://anima-uploads.s3.amazonaws.com/projects/64e665a7f1b61250aab168b5/releases/64e667027704051b825b49eb/img/rectangle-22143-1@2x.png"
+                />
+                <img
+                    className="w-[348px] h-[348px] left-[546px] top-[2303px] absolute rounded-[30px] shadow"
+                    src="https://anima-uploads.s3.amazonaws.com/projects/64e665a7f1b61250aab168b5/releases/64e667027704051b825b49eb/img/rectangle-22144-1@2x.png"
+
+                />
+                <img
+                    className="w-[348px] h-[348px] left-[958px] top-[2303px] absolute rounded-[30px] shadow"
+                    src="https://anima-uploads.s3.amazonaws.com/projects/64e665a7f1b61250aab168b5/releases/64e667027704051b825b49eb/img/rectangle-22145-1@2x.png"
+
+                />
+                <div className="w-[1440px] h-[401px] left-0 top-[2751px] absolute">
+                    <div className="w-[1440px] h-[401px] left-0 top-0 absolute bg-rose-800" />
+                    <img
+                        className="w-[331px] h-[331px] left-[961px] top-[31px] absolute opacity-70"
+                        src="https://anima-uploads.s3.amazonaws.com/projects/64e665a7f1b61250aab168b5/releases/64e667027704051b825b49eb/img/lovepik-com-401403517-traditional-pattern-holiday-material-1-1@2x.png"
+                    />
+                    <a href="#" className="left-[478px] top-[66px] absolute text-justify text-white text-xl font-semibold">
+                        Home
+                    </a>
+                    <a href="#" className="left-[687px] top-[66px] absolute text-justify text-white text-xl font-semibold">
+                        Profile
+                    </a>
+                    <a href="#" className="left-[478px] top-[128px] absolute text-justify text-white text-xl font-semibold">
+                        Benefits
+                    </a>
+                    <a href="#" className="left-[687px] top-[128px] absolute text-justify text-white text-xl font-semibold">
+                        Upload Opportunities
+                    </a>
+                    <a href="#" className="left-[478px] top-[190px] absolute text-justify text-white text-xl font-semibold">
+                        Service
+                    </a>
+                    <a href="#" className="left-[687px] top-[190px] absolute text-justify text-white text-xl font-semibold">
+                        View Artist
+                    </a>
+                    <a href="#" className="left-[478px] top-[252px] absolute text-justify text-white text-xl font-semibold">
+                        Gallery
+                    </a>
+                    <a href="#" className="left-[687px] top-[252px] absolute text-justify text-white text-xl font-semibold">
+                        Applications
+                    </a>
+                    <a href="#" className="left-[478px] top-[314px] absolute text-justify text-white text-xl font-semibold">
+                        Events
+                    </a>
+                    <a href="#" className="left-[687px] top-[314px] absolute text-justify text-white text-xl font-semibold">
+                        Application Updates
+                    </a>
+                    <div className="w-[293px] h-[46px] left-[50px] top-[255px] absolute text-center">
+                        <span style={{color:"orange"}}>eKa</span>
+                        <span style={{color:"blue"}}>lak</span>
+                        <span style={{color:"green"}}>aar</span>
+                    </div>
+                    <img
+                        className="w-[195px] h-[143px] left-[112px] top-[80px] absolute"
+                        src="https://anima-uploads.s3.amazonaws.com/projects/64e665a7f1b61250aab168b5/releases/64e667027704051b825b49eb/img/ekalakaa-7-1@2x.png"
+                    />
+                    <div className="left-[50px] top-[371px] absolute text-center">
+                        
+                        <div className="text-center" >
+                           
+                            <span className='text-sm font-medium tracking-[1px]' style={{color:"white" }}>
+                            ©2023 eKalakaar, All Right Reserved 
+                            </span>
+                        </div>
+                    </div>
+                    <div className="w-[100px] h-[401px] left-[1340px] top-0 absolute bg-rose-300" />
+                    <div className="w-[50px] h-[50px] left-[1365px] top-[41px] absolute bg-white rounded-full" />
+                    <div className="w-[50px] h-[50px] left-[1365px] top-[131px] absolute bg-white rounded-full" />
+                    <div className="w-[50px] h-[50px] left-[1365px] top-[221px] absolute bg-white rounded-full" />
+                    <div className="w-[50px] h-[50px] left-[1365px] top-[311px] absolute bg-white rounded-full" />
+                    <img
+                        className="w-8 h-8 left-[1374px] top-[50px] absolute"
+                        src="https://anima-uploads.s3.amazonaws.com/projects/64e665a7f1b61250aab168b5/releases/64e667027704051b825b49eb/img/instagram-2@2x.png"
+
+                    />
+                    <img
+                        className="w-8 h-8 left-[1374px] top-[140px] absolute"
+                        src="https://anima-uploads.s3.amazonaws.com/projects/64e665a7f1b61250aab168b5/releases/64e667027704051b825b49eb/img/facebook-2@2x.png"
+
+                    />
+                    <img
+                        className="w-8 h-8 left-[1374px] top-[230px] absolute"
+                        src="https://anima-uploads.s3.amazonaws.com/projects/64e665a7f1b61250aab168b5/releases/64e667027704051b825b49eb/img/twitterx-1@2x.png"
+
+                    />
+                    <img
+                        className="w-8 h-8 left-[1374px] top-[320px] absolute"
+                        src="https://anima-uploads.s3.amazonaws.com/projects/64e665a7f1b61250aab168b5/releases/64e667027704051b825b49eb/img/youtube-3@2x.png"
+
+                    />
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Page
